3f5e7919e63f8b6f9ea59e613c4a4fd83958ff39
prince <prince.bc@mitrahsoft.com>
2024-12-19 12:46:00 +0530

    I have changed the url from compass to atlas

--- initDB.js
+++ initDB.js
@@ -1,14 +1,32 @@
 const mongoose = require("mongoose");
 
+const dbUrl = "mongodb+srv://prince:TZiwzJ2R5oDhdVmA@prince-shoppify-cluster.gmyuu.mongodb.net/?retryWrites=true&w=majority&appName=prince-shoppify-cluster"
+
+const connectionParams = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
+
 module.exports = () => {
+
+  
   mongoose
-    .connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME })
+    .connect(dbUrl, connectionParams)
     .then(() => {
       console.log("Mongodb database is connected...");
     })
     .catch((err) => {
       console.log(err.message);
     });
+
+  // mongoose
+  //   .connect(process.env.MONGODB_URI, { dbName: process.env.DB_NAME })
+  //   .then(() => {
+  //     console.log("Mongodb database is connected...");
+  //   })
+  //   .catch((err) => {
+  //     console.log(err.message);
+  //   });
 
   ///***mongoose events */
   mongoose.connection.on("connected", () => {
