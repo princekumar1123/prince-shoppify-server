50b8218a59317f0939f8f165367dc28795500b5e
prince <prince.bc@mitrahsoft.com>
2024-12-20 17:45:25 +0530

    I have added the product crud

--- controllers/AdminController/ProductsController.js
+++ controllers/AdminController/ProductsController.js
@@ -1,40 +1,108 @@
+// const createError = require("http-errors");
+// const mongoose = require("mongoose");
+
+// const productData = require('../../models/AdminModel/productsData')
+
+// module.exports = ({
+//     getAllProduct: async (req, res, next) => {
+//         const data = await productData.find({}, { _v: 0 })
+
+//         console.log(data);
+
+//         res.send(data)
+
+
+//     },
+//     getProductById: async (req, res, next) => {
+//         const paramsID = req.params.id;
+//         const particularData = await productData.findById(paramsID)
+//         res.send(particularData)
+//     },
+
+//     addProduct: async (req, res, next) => {
+
+//         const products = new productData(req.body)
+
+//         const result = await products.save()
+
+//         console.log(result)
+
+//         res.send(result)
+
+//     },
+//     updateProduct: async (req, res, next) => {
+
+//         const id = req.params.id
+//         const update = req.body
+//         const option = { new: true }
+//         const result = await productData.findByIdAndUpdate(id, update, option)
+
+//         res.send(result)
+
+//     },
+
+//     deleteProduct: async (req, res, next) => {
+//         const id = req.params.id
+
+//         const result = await productData.findByIdAndDelete
+
+//         res.send(result)
+//     },
+
+//     sample: async (req, res, next) => {
+//         console.log("ugfuug");
+
+//         res.send({ data: "Hi prince" })
+
+//     }
+// });
+
+
+
+
+
 const createError = require("http-errors");
-const mongoose = require("mongoose");
-
-const productData = require('../../models/AdminModel/productsData')
-
-module.exports = ({
-    getAllProduct: async (req, res, next) => {
-        const data = await productData.find({}, { _v: 0 })
-
-        console.log(data);
-
-        res.send(data)
-
-
-    },
-    getProductById:async(req,res,next)=>{
-        const paramsID = req.params.id;
-        const particularData = await productData.findById(paramsID)
-        res.send(particularData)
-    },
-
-    addProduct: async (req, res, next) => {
-
-        const products = new productData(req.body)
-
-        const result = await products.save()
-
-        console.log(result)
-
-        res.send(result)
-
-    },
-
-    sample: async (req, res, next) => {
-        console.log("ugfuug");
-        
-        res.send({data:"Hi prince"})
-
+const handleAsync = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        next(error);
     }
-});
+};
+
+module.exports = {
+    getAllProducts: handleAsync(async (req, res) => {
+        const products = await ProductData.find({}, { __v: 0 });
+        res.status(200).json(products);
+    }),
+
+    getProductById: handleAsync(async (req, res) => {
+        const { id } = req.params;
+        const product = await ProductData.findById(id);
+        if (!product) throw createError(404, "Product not found");
+        res.status(200).json(product);
+    }),
+
+    addProduct: handleAsync(async (req, res) => {
+        const product = new ProductData(req.body);
+        const savedProduct = await product.save();
+        res.status(201).json(savedProduct);
+    }),
+
+    updateProduct: handleAsync(async (req, res) => {
+        const { id } = req.params;
+        const update = req.body;
+        const options = { new: true, runValidators: true };
+        const updatedProduct = await ProductData.findByIdAndUpdate(id, update, options);
+        if (!updatedProduct) throw createError(404, "Product not found");
+        res.status(200).json(updatedProduct);
+    }),
+
+    deleteProduct: handleAsync(async (req, res) => {
+        const { id } = req.params;
+        const deletedProduct = await ProductData.findByIdAndDelete(id);
+        if (!deletedProduct) throw createError(404, "Product not found");
+        res.status(200).json({ message: "Product deleted successfully" });
+    }),
+};
+
--- routers/AdminRouter/adminRouts.js
+++ routers/AdminRouter/adminRouts.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const adminController = require('../../controllers/AdminController/ProductsController')
 
-router.post("/addproduct",adminController.addProduct)
+router.post("/addproduct", adminController.addProduct)
 
-router.get("/getproducts",adminController.getAllProduct)
+router.get("/getproducts", adminController.getAllProducts)
 
-router.get("/abc",adminController.sample)
+// router.get("/abc", adminController.sample)
 
-router.get("/getproductbyid/:id",adminController.getProductById)
+router.get("/getproductbyid/:id", adminController.getProductById)
+
+router.put("/updateproduct/:id", adminController.updateProduct)
+
+router.delete("/deleteproduct/:id", adminController.deleteProduct)
 
 module.exports = router;
